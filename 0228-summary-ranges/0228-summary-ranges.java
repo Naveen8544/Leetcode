c4e0cca2004f4cd7982014e785dd4c0a402b7c33
Naveen Sharma <naveensharmaaurangabad@gmail.com>
2025-09-22 22:56:02 +0530

    Time: 6 ms (26.96%), Space: 41.7 MB (86.04%) - LeetHub

---
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> result = new ArrayList<>();
+          if (nums.length == 0) return result;
+
+        int start = nums[0];
+
+        for (int i = 1; i <= nums.length; i++) {
+            // Check if we reached the end or the next number is not consecutive
+            if (i == nums.length || nums[i] != nums[i - 1] + 1) {
+                if (start == nums[i - 1]) {
+                    result.add(String.valueOf(start));
+                } else {
+                    result.add(start + "->" + nums[i - 1]);
+                }
+
+                // Start a new range
+                if (i < nums.length) {
+                    start = nums[i];
+                }
+            }
+        }
+
+        return result;
+    }
+}
